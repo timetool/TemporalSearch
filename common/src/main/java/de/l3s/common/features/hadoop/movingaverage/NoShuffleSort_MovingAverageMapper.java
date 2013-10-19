b5e5ab48332edf36dce252e81b1278b8c3f9a6d0
nattiya <nattiya@gmail.com>
2013-10-19 16:07:33 +0200

    new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/MovingAverageJob.java 	new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/MovingAverageMapper.java 	new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/MovingAverageReducer.java 	new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/NoShuffleSort_MovingAverageJob.java 	new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/NoShuffleSort_MovingAverageMapper.java 	new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/NoShuffleSort_MovingAverageReducer.java 	new file:   common/src/main/java/de/l3s/common/features/hadoop/movingaverage/SlidingWindow.java 	new file:   common/src/main/java/de/l3s/common/models/timeseries/CompositeKeyComparator.java 	new file:   common/src/main/java/de/l3s/common/models/timeseries/NaturalKeyGroupingComparator.java 	new file:   common/src/main/java/de/l3s/common/models/timeseries/NaturalKeyPartitioner.java 	new file:   common/src/main/java/de/l3s/common/models/timeseries/TimeseriesDataPoint.java 	new file:   common/src/main/java/de/l3s/common/models/timeseries/TimeseriesKey.java

---
+++ common/src/main/java/de/l3s/common/features/hadoop/movingaverage/NoShuffleSort_MovingAverageMapper.java
@@ -0,0 +1,68 @@
+package de.l3s.common.features.hadoop.movingaverage;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.log4j.Logger;
+
+public class NoShuffleSort_MovingAverageMapper  extends MapReduceBase implements Mapper<LongWritable, Text, Text, TimeseriesDataPoint> 
+{
+
+	   
+	static enum Timeseries_Counters { BAD_PARSE, BAD_LOOKUP };
+
+	   
+	   private JobConf configuration;
+	   private final Text key = new Text();
+	   private final TimeseriesDataPoint val = new TimeseriesDataPoint();
+	   
+
+	   private static final Logger logger = Logger.getLogger( NoShuffleSort_MovingAverageMapper.class );
+
+	   
+	   public void close() {
+		   
+		   
+	   }
+	   
+	   public void configure(JobConf conf) {
+	      this.configuration = conf;
+	      
+	   }
+	   
+	   
+	   
+	   
+	   @Override
+	   public void map(LongWritable inkey, Text value, OutputCollector<Text, TimeseriesDataPoint> output, Reporter reporter) throws IOException {
+
+	      String line = value.toString();
+	      
+	      YahooStockDataPoint rec = YahooStockDataPoint.parse( line );
+	      
+	      if (rec != null) {
+	    	  
+	    		  // set both parts of the key
+	    		  key.set( rec.stock_symbol );
+	    	      
+	    		  val.fValue = rec.getAdjustedClose();
+	    	      val.lDateTime = rec.date;
+	    	      
+	    	      // now that its parsed, we send it through the shuffle for sort, onto reducers
+	    	      output.collect(key, val);
+	         
+	      } else {
+	    	  
+	    	  reporter.incrCounter( Timeseries_Counters.BAD_PARSE, 1 );
+	    	  
+	      }
+	    	  
+	   }
+
+}
---
+++ common/src/main/java/de/l3s/common/models/timeseries/TimeseriesDataPoint.java
@@ -0,0 +1,100 @@
+package de.l3s.common.models.timeseries;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import org.apache.hadoop.io.Writable;
+
+/**
+ * 
+ * TimeseriesDataPoint
+ * 
+ * The basic value or point type in the Map Reduce application.
+ * 
+ * @author jpatterson
+ * 
+ */
+public class TimeseriesDataPoint implements Writable,
+		Comparable<TimeseriesDataPoint> {
+	// , Comparable
+	public long lDateTime;
+	public float fValue;
+
+	private static final String DATE_FORMAT = "yyyy-MM-dd";
+	private static SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
+
+	/**
+	 * Deserializes the point from the underlying data.
+	 * 
+	 * @param in
+	 *            A DataInput object to read the point from.
+	 * @see java.io.DataInput
+	 * @see org.apache.hadoop.io.Writable#readFields(java.io.DataInput)
+	 * 
+	 */
+	public void readFields(DataInput in) throws IOException {
+
+		this.lDateTime = in.readLong();
+		this.fValue = in.readFloat();
+	}
+
+	/**
+	 * This is a static method that deserializes a point from the underlying
+	 * binary representation.
+	 * 
+	 * @param in
+	 *            A DataInput object that represents the underlying stream to
+	 *            read from.
+	 * @return A TimeseriesDataPoint
+	 * @throws IOException
+	 */
+	public static TimeseriesDataPoint read(DataInput in) throws IOException {
+
+		TimeseriesDataPoint p = new TimeseriesDataPoint();
+		p.readFields(in);
+		return p;
+
+	}
+
+	public String getDate() {
+
+		return sdf.format(this.lDateTime);
+
+	}
+
+	public void copy(TimeseriesDataPoint source) {
+
+		this.lDateTime = source.lDateTime;
+		this.fValue = source.fValue;
+
+	}
+
+	@Override
+	public void write(DataOutput out) throws IOException {
+
+		out.writeLong(this.lDateTime);
+		out.writeFloat(this.fValue);
+
+	}
+
+	/**
+	 * This is only used in the case of manually sorting the data in the reducer
+	 * 
+	 * Map Reduce itself does not use this method for sorting the data.
+	 * 
+	 */
+	@Override
+	public int compareTo(TimeseriesDataPoint oOther) {
+		if (this.lDateTime < oOther.lDateTime) {
+			return -1;
+		} else if (this.lDateTime > oOther.lDateTime) {
+			return 1;
+		}
+
+		// default -- they are equal
+		return 0;
+	}
+
+}
